b322a3614bec1c9fb48ac0099b0302da887c7ca9
NiconicoLab <lanesra.xxx@gmail.com>
2021-01-18 22:23:16 +0900

    add program of execute exe

--- bash/main.c
+++ bash/main.c
@@ -6,6 +6,16 @@ int main(int argc, char* argv[])
 	printf("bash start\n");
 	system("bash script.sh");
 	printf("bash end\n");
-	
+
+	char program[] = "./hello"; // 実行する別プログラム名
+	int result = system(program); // 別プログラムを実行
+	if ( result == EXIT_SUCCESS )
+	{
+		printf("正常に実行できました\n");
+	} else
+	{
+		printf("正常に実行できませんでした\n");
+	}
+
 	return 0;
 }
