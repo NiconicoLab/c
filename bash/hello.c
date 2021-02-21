ce4e5ab6006db706f614e2f721b690dc63e5ac37
NiconicoLab <lanesra.xxx@gmail.com>
2021-02-21 19:23:55 +0900

    modify execute program

--- bash/hello.c
+++ bash/hello.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
-int main()
+int main(int argc, char *argv[])
 {
+	if(argc != 1) {
+		printf("Usage$%s\n", argv[0]);
+		return 1;
+	}
+
 	printf("Hello world\n");
+
 	return 0;
 }
