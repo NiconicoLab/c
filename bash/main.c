40cd8720c83d4a4aa30973d0a2aa3cee910b18fb
NiconicoLab <lanesra.xxx@gmail.com>
2021-02-21 19:34:24 +0900

    modify call to script and exe

--- bash/main.c
+++ bash/main.c
@@ -7,8 +7,18 @@ int main(int argc, char* argv[])
 	system("bash script.sh");
 	printf("bash end\n");
 
-	char program[] = "./hello"; // 実行する別プログラム名
-	int result = system(program); // 別プログラムを実行
+	char program1[] = "./hello"; // 実行する別プログラム名
+	int result = system(program1); // 別プログラムを実行
+	if ( result == EXIT_SUCCESS )
+	{
+		printf("正常に実行できました\n");
+	} else
+	{
+		printf("正常に実行できませんでした\n");
+	}
+
+	char program2[] = "./hello test"; // 実行する別プログラム名
+	result = system(program2); // 別プログラムを実行
 	if ( result == EXIT_SUCCESS )
 	{
 		printf("正常に実行できました\n");
