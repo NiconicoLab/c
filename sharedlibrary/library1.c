a1fc0f0c976820c5d13afa5bf91400347f4360f2
NiconicoLab <lanesra.xxx@gmail.com>
2021-08-20 21:43:52 +0900

    add description

--- sharedlibrary/library1.c
+++ sharedlibrary/library1.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "library1.h"
 
+// UnixやLinuxのC言語で共有ライブラリ(.so)を作成
+
 int library_func(int i)
 {
 	printf("call library func\n");
--- sharedlibrary/library1.h
+++ sharedlibrary/library1.h
@@ -1,6 +1,9 @@
 #ifndef __LIBRARY1_H__
 #define __LIBRARY1_H__
 
+// CとC++の両方のプログラムにインクルード可能なヘッダーファイルを作成するために
+// __cplusplusの定義の有無またはその値に基づいた条件付きコンパイルを使用する
+
 #if defined (__cplusplus)
 extern "C" {
 #endif
