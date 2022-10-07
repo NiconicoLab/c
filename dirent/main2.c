a2eb2243e3f604ee75f171fcf4a62b647218d373
NiconicoLab <lanesra.xxx@gmail.com>
2022-10-07 12:00:00 +0900

    separate function

--- dirent/main2.c
+++ dirent/main2.c
@@ -2,6 +2,7 @@
 #include <dirent.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #if 0 // glicbで定義されている構造体
 struct dirent {
@@ -13,11 +14,14 @@ struct dirent {
 };
 #endif
 
-int main(int argc, char *argv[])
+#define MAX_CREATE_FILE_NUM 5
+const char* fix_fname = "_aaa.txt";
+
+int get_file(char *fname_create, char *fname_delete)
 {
 	DIR *dir = opendir("./");
 	struct dirent *dp;
-
+	
 	unsigned int cnt = 1;
 
 	printf("---------Hit File List---------\n");
@@ -25,9 +29,7 @@ int main(int argc, char *argv[])
 	cnt = 0;
 	for(dp = readdir(dir); dp != NULL; dp = readdir(dir))
 	{
-		char *s = "_aaa.txt";
-		// "aaa"という文字列を検索する
-		char *found = strstr(dp->d_name, s);
+		char *found = strstr(dp->d_name, fix_fname);
 		if(found) {
 			printf("%d : %s :", cnt, dp->d_name);
 			dp->d_name[4] = '\0';
@@ -37,6 +39,12 @@ int main(int argc, char *argv[])
 		}
 	}
 	closedir(dir);
+
+	if(0 == cnt)
+	{
+		return -1;
+	}
+
 	int sum, i, j;
 	int total = cnt;
 	for(i=0;i<total;++i)
@@ -56,9 +64,8 @@ int main(int argc, char *argv[])
 	for (int i = 0; i < total; i++) {
 		printf("%d\n", number[i]);
 	}
-	printf("---\n");
 
-	printf("--- result ---\n");
+	printf("---------Judge fname---------\n");
 	int over = 0;
 	if(999 == number[total-1]) {
 		for (int i = total-1; i >= 0; i--) {
@@ -70,12 +77,65 @@ int main(int argc, char *argv[])
 		// printf("over = %d\n", over);
 		if(over) {
 			printf("delete %04d_aaa.txt : create %04d_aaa.txt\n", number[total-over], number[total-over-1]+1);
+			sprintf(fname_delete, "%04d%s", number[total-over], fix_fname);
+			sprintf(fname_create, "%04d%s", number[total-over-1]+1, fix_fname);
 		}
 		else {
 		}
 	}
 	else {
-		printf("delete %04d_aaa.txt : create %04d_aaa.txt\n", number[0], number[total-1]+1);
+		printf("delete %04d%s : create %04d%s\n", number[0], fix_fname, number[total-1]+1, fix_fname);
+		sprintf(fname_delete, "%04d%s", number[0], fix_fname);
+		sprintf(fname_create, "%04d%s", number[total-1]+1, fix_fname);
+	}
+
+	if(total >= MAX_CREATE_FILE_NUM)
+	{
+		// printf("%d\n", total);
+		return 1;
+	}
+	return 0;
+}
+
+
+
+
+int main(int argc, char *argv[])
+{
+	char fname_create[50];
+	char fname_delete[50];
+
+	bool create_flag = false;
+	bool delete_flag = false;
+	
+	int ret = get_file(fname_create, fname_delete); 
+	if(-1 == ret)
+	{
+		sprintf(fname_create, "%04d%s", 0, fix_fname);
+		delete_flag = false;
+		create_flag = true;
+	}
+	else if(0 == ret)
+	{
+		delete_flag = false;
+		create_flag = true;
+		// printf("delete %s : create %s\n", fname_delete, fname_create);
+	}
+	else // 1 == ret
+	{
+		delete_flag = true;
+		create_flag = true;
+		// printf("delete %s : create %s\n", fname_delete, fname_create);
+	}
+
+	printf("---------Main program---------\n");
+	if(true == create_flag)
+	{
+		printf("create %s\n", fname_create);
+	}
+	if(true == delete_flag)
+	{
+		printf("delete %s\n", fname_delete);
 	}
 
 	return 0;
